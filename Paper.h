5d8157ddf2cfdc775e37160b71b040907787b3b4
lilrooness <lilrooness@users.noreply.github.com>
2024-07-28 20:16:01 +0000

    add getCard/s & getStack/s functions to API

--- Paper.h
+++ Paper.h
@@ -14,6 +14,18 @@ typedef void(stack_move_callback_fun)(
 	void* data
 );
 
+typedef struct {
+	int id;
+	char* name;
+	char* parentName;
+} Paper_Card;
+
+typedef struct {
+	int id;
+	int ncards;
+	Paper_Card* cards;
+} Paper_Stack;
+
 class Program;
 
 PAPER_API Program* Paper_newProgram();
@@ -27,3 +39,11 @@ PAPER_API void Paper_runSetup(Program* program);
 PAPER_API void Paper_runTurn(Program* program);
 
 PAPER_API void Paper_setStackMoveCallback(Program* program, stack_move_callback_fun* f, void* data);
+
+PAPER_API int Paper_getStack(Program* program, int id, Paper_Stack* stack_out);
+
+PAPER_API int Paper_getCard(Program* program, int id, Paper_Card* card_out);
+
+PAPER_API Paper_Card* Paper_getAllCards(Program* program, int* ncards_out);
+
+PAPER_API int* Paper_getAllStackIDs(Program* program, int* nstacks_out);
